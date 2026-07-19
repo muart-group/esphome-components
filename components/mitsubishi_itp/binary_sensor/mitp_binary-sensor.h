fcd5db34fce9a4c7f11da82e2e0f137d4f5367ff
Sam <467704+samphonic@users.noreply.github.com>
2026-07-19 20:12:11 +0000

    Don't count MHK2 re-sends as commands received.

--- components/mitsubishi_itp/binary_sensor/mitp_binary-sensor.h
+++ components/mitsubishi_itp/binary_sensor/mitp_binary-sensor.h
@@ -49,19 +49,27 @@ class UsingInternalTemperatureSensor : public MITPBinarySensor {
   void using_internal_temperature(const bool using_internal) override { mitp_binary_sensor_state_ = using_internal; }
 };
 class ThermostatCommandReceivedSensor : public MITPBinarySensor {
-  // TODO: This needs to be the correct packet (but we don't forward those, so we might need)
-  // a separate call a la using_internal_temperature
-  void receive_packet(const SettingsSetRequestPacket &packet) override { mitp_binary_sensor_state_ = true; }
+  void receive_packet(const SettingsGetResponsePacket &packet) override;
+  void receive_packet(const SettingsSetRequestPacket &packet) override;
 
   void publish() override {
     if (mitp_binary_sensor_state_.has_value())
       // Binary sensors automatically dedup publishes (I think) and so will only actually publish on change
       publish_state(mitp_binary_sensor_state_.value());
     // We only want this sensor to briefly jump to true when a command is received, and then back to false
+    // its state will get published next time publish() is called.
     if (mitp_binary_sensor_state_) {
       mitp_binary_sensor_state_ = false;
     }
   }
+ private:
+  bool has_last_packet_ = false;
+  uint8_t last_power_;
+  uint8_t last_mode_;
+  float last_target_temp_;
+  uint8_t last_fan_;
+  uint8_t last_vane_;
+  uint8_t last_horizontal_vane_;
 };
 
 }  // namespace mitsubishi_itp
